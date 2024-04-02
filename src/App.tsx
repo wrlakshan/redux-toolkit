e69c788c68908d02321187f7cebaf56926076d0c
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-04-02 08:16:02 +0530

    add lottie animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment, incrementByAmount } from "./redux/counter.js";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import owl from "./assets/owl.json";
 
 function App() {
   const { count } = useSelector((state) => state.counter);
@@ -13,8 +16,26 @@ function App() {
     console.log(state);
   }, [state]);
 
+  const style = {
+    height: 400,
+  };
+
+  const owlRef = useRef<LottieRefCurrentProps>(null);
+
   return (
     <>
+      <div>
+        <Lottie
+          animationData={owl}
+          lottieRef={owlRef}
+          style={style}
+          loop={true}
+          onComplete={() => {
+            owlRef.current?.setDirection(-1);
+            owlRef.current?.play();
+          }}
+        />
+      </div>
       <h1>Number Value: {count} </h1>
       <div className="card">
         <button onClick={() => dispatch(decrement())}>Decrement -</button>
